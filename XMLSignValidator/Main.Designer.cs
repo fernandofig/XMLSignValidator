f9b43cf937752a8ee0f997c77009a9144b1ff277
Luiz Fernando Figueiredo <luiz.figueiredo@embrateledi.com.br>
2019-03-26 23:39:19 -0300

    Init

---
+++ XMLSignValidator/Main.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace XMLSignValidator {
+	partial class Main {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.DropArea = new System.Windows.Forms.Panel();
+			this.Result = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// DropArea
+			// 
+			this.DropArea.AllowDrop = true;
+			this.DropArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.DropArea.Location = new System.Drawing.Point(12, 12);
+			this.DropArea.Name = "DropArea";
+			this.DropArea.Size = new System.Drawing.Size(324, 86);
+			this.DropArea.TabIndex = 0;
+			this.DropArea.DragDrop += new System.Windows.Forms.DragEventHandler(this.XmlDrop);
+			this.DropArea.DragOver += new System.Windows.Forms.DragEventHandler(this.XmlDragging);
+			// 
+			// Result
+			// 
+			this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.Result.Location = new System.Drawing.Point(12, 110);
+			this.Result.Name = "Result";
+			this.Result.Size = new System.Drawing.Size(324, 25);
+			this.Result.TabIndex = 1;
+			this.Result.Text = "Arraste um XML para a área acima!";
+			this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(348, 145);
+			this.Controls.Add(this.Result);
+			this.Controls.Add(this.DropArea);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Main";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Validador de Assinatura de XMLs";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel DropArea;
+		private System.Windows.Forms.Label Result;
+	}
+}
+
---
+++ XMLSignValidator/Main.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+
+namespace XMLSignValidator {
+	public partial class Main : Form {
+		public Main() {
+			InitializeComponent();
+		}
+
+		private void ValidaXml(string arquivo) {
+			try {
+				XmlDocument x = new XmlDocument();
+				x.Load(arquivo);
+
+				bool valid = SignVerify(x);
+
+				if (valid) {
+					Result.Text = "Válido";
+					Result.ForeColor = Color.Green;
+				} else {
+					Result.Text = "Inválido";
+					Result.ForeColor = Color.Red;
+				}
+			} catch (Exception e) {
+				if (e is XmlException) {
+					MessageBox.Show("Erro ao parsear o arquivo como XML.\n\nEste arquivo é um XML válido?", "Erro!");
+				} else {
+					MessageBox.Show(e.GetBaseException().Message, "Erro!");
+				}
+			}
+		}
+
+		private void XmlDrop(object sender, DragEventArgs e) {
+			var dropped = ((string[])e.Data.GetData(DataFormats.FileDrop));
+			var files = dropped.ToList();
+
+			if (!files.Any())
+				return;
+
+			if (files.Count > 1) {
+				MessageBox.Show("Arraste apenas 1 arquivo!", "Atenção");
+				return;
+			}
+
+			FileAttributes attr = File.GetAttributes(files[0]);
+
+			if (attr.HasFlag(FileAttributes.Directory)) {
+				MessageBox.Show("Esta aplicação aceita apenas arquivos XML", "Atenção");
+				return;
+			}
+
+			if (Path.GetExtension(files[0]).ToLower() != ".xml") {
+				MessageBox.Show("Esta aplicação aceita apenas arquivos XML", "Atenção");
+				return;
+			}
+
+			ValidaXml(files[0]);
+		}
+
+		private void XmlDragging(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+		}
+
+		private static bool SignVerify(XmlDocument document) {
+			if (document == null) throw new ArgumentNullException(nameof(document), "XML document is null.");
+
+			SignedXml signed = new SignedXml(document);
+			XmlNodeList list = document.GetElementsByTagName("Signature");
+			if (list == null)
+				throw new CryptographicException($"O XML não possui uma assinatura.");
+			if (list.Count > 1)
+				throw new CryptographicException($"O XML contém mais de uma assinatura.");
+
+			signed.LoadXml((XmlElement)list[0]);
+
+			List<X509Certificate2> certs = new List<X509Certificate2>();
+
+			foreach (KeyInfoX509Data clause in signed.KeyInfo) {
+				foreach (X509Certificate2 cert in clause.Certificates) {
+					certs.Add(cert);
+				}
+			}
+
+			if (certs.Count == 0) throw new CryptographicException($"O XML não contém um certificado.");
+
+			if (certs.Count > 1) throw new CryptographicException($"O XML contém mais de um certificado.");
+
+			return signed.CheckSignature(certs[0], true);
+		}
+	}
+}
